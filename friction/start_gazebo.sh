92d3ee90ae8b9616b0b7aeecca30de457fc04368
Allison Thackston <allison@allisonthackston.com>
2021-06-18 08:24:23 -0700

    update friction pyramid

--- friction/start_gazebo.sh
+++ friction/start_gazebo.sh
@@ -2,5 +2,5 @@
 
 set -e 
 
-xacro --xacro-ns -o friction_test.gazebo friction_test.gazebo.xacro
+xacro -o friction_test.gazebo friction_test.gazebo.xacro
 gazebo --verbose --pause friction_test.gazebo
--- friction/start_ign.sh
+++ friction/start_ign.sh
@@ -2,5 +2,5 @@
 
 set -e 
 
-xacro --xacro-ns -o friction_test.ign friction_test.ign.xacro
+xacro -o friction_test.ign friction_test.ign.xacro
 ign gazebo --verbose 4 friction_test.ign
